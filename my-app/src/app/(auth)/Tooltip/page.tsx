ff7fde3b8fb19886a20095f9dd1aaa405d885684
hanwoojin <hanwoojin3@muplus.co.kr>
2025-05-16 17:21:40 +0900

    Tooltip 추가

--- my-app/src/app/(auth)/Tooltip/page.tsx
+++ my-app/src/app/(auth)/Tooltip/page.tsx
@@ -1,15 +1,67 @@
 'use client';
 
 import React from 'react';
-import { Button, ConfigProvider, Flex, Tooltip } from 'antd';
+import { Button, ConfigProvider, Flex, Tooltip, Divider, Space } from 'antd';
 
 const text = <span>prompt text</span>;
 
 const buttonWidth = 80;
 
 export default function TooltipPage () {
+
+    const ComponentWithEvents: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => (
+        <span {...props}>This text is inside a component with the necessary events exposed.</span>
+    );
+
+    const colors = [
+        'pink',
+        'red',
+        'yellow',
+        'orange',
+        'cyan',
+        'green',
+        'blue',
+        'purple',
+        'geekblue',
+        'magenta',
+        'volcano',
+        'gold',
+        'lime',
+    ];
+
+    const customColors = ['#f50', '#2db7f5', '#87d068', '#108ee9'];
+
     return (
         <div className='section'>
+            <Tooltip title="prompt text">
+                <ComponentWithEvents />
+            </Tooltip>
+            <br />
+            <br />
+            <hr />
+            <br />
+            <Divider orientation="left">Presets</Divider>
+            <Space wrap>
+            {colors.map((color) => (
+                <Tooltip title="prompt text" color={color} key={color}>
+                <Button>{color}</Button>
+                </Tooltip>
+            ))}
+            </Space>
+            <Divider orientation="left">Custom</Divider>
+            <Space wrap>
+            {customColors.map((color) => (
+                <Tooltip title="prompt text" color={color} key={color}>
+                <Button>{color}</Button>
+                </Tooltip>
+            ))}
+            </Space>
+            <br />
+            <br />
+            <br />
+            <hr />
+            <br />
+            <br />
             <ConfigProvider button={{ style: { width: buttonWidth, margin: 4 } }}>
                 <Flex vertical justify="center" align="center" className="demo">
                 <Flex justify="center" align="center" style={{ whiteSpace: 'nowrap' }}>
